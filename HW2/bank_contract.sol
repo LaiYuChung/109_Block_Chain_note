cb3bb70d7b1be9b7890ee21b0e36da47313c54bc
LaiYuChung <55571197+LaiYuChung@users.noreply.github.com>
2020-04-09 09:30:38 +0800

    Update bank_contract.sol

--- HW2/bank_contract.sol
+++ HW2/bank_contract.sol
@@ -7,14 +7,13 @@ contract bank_contract {
     mapping (address => uint256) public balances; //地址映射到存款金額
     address payable owner; //銀行的擁有者，會在constructor做設定
 
-//設定owner為創立合約的人
+//設定owner為創立合約的人--ok
     constructor() public payable {
         owner = msg.sender;
     }
 //可以讓使用者call這個函數把錢存進合約地址，並且在balances中紀錄使用者的帳戶金額
-    function deposit(address studentAddress,uint256 put_money) public payable {
-        put_money = msg.value;
-        balances[studentAddress] = put_money;
+    function deposit(address studentAddress) public payable {
+        balances[studentAddress] = msg.value;
     }
     
 //可以讓使用者從合約提錢，這邊需要去確認合約裡的餘額 >= 想提的金額
@@ -25,31 +24,34 @@ contract bank_contract {
 
 //可以讓使用者從合約轉帳給某個地址，這邊需要去確認合約裡的餘額 >= 想轉的金額
 //實現的是銀行內部轉帳，也就是說如果轉帳成功balances的目標地址會增加轉帳金額
-    function transfer(uint256 transferamount , address studentsAddress) public payable {
-        require(transferamount < balances[studentsAddress],"not enough to transfer!");
+    function transfer(uint256 transferamount, address studentsAddress) public payable {
+        require(transferamount <= balances[studentsAddress],"not enough to transfer!");
         balances[studentsAddress] = transferamount;
     }
 
-//從balances回傳使用者的銀行帳戶餘額
-    function getBalance(address studentAddress) external view returns(uint256){
+//從balances回傳使用者的銀行帳戶餘額--ok
+    function getBalance(uint256 current_balance) external returns(address studentAddress){
         require(owner == msg.sender, "permission denied");
-        return balances[studentAddress];
+        balances[msg.sender] = current_balance;
+        return msg.sender;
     }
     
 //回傳銀行合約的所有餘額，設定為只有owner才能呼叫成功
-    function getBankBalance(address studentAddress) public view returns(uint256){
+    function getBankBalance(uint256 all_balance) public returns(address studentAddress){
         require(owner == msg.sender, "permission denied");
-        return balances[studentAddress];
+        balances[msg.sender] = all_balance;
+        return msg.sender;
     }
 
-//透過students把學號映射到使用者的地址
+//透過students把學號映射到使用者的地址--ok
     function enroll(string memory studentId, address studentAddress) public {
         students[studentId] = studentAddress;
     }
     
-//當觸發fallback時，檢查觸發者是否為owner，是則自殺合約，把合約剩餘的錢轉給owner
+//當觸發fallback時，檢查觸發者是否為owner，是則自殺合約，把合約剩餘的錢轉給owner--ok
     fallback() external {
         require(owner == msg.sender, "permission denied");
         selfdestruct(owner);
     }
 }
+
